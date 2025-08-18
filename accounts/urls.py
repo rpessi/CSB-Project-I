056f2c6a272f32007552ab83a046c6da0175aa5f
Riitta Pessi <riitta@pessi.fi>
2025-08-18 15:17:00 +0300

    Add email update with SQL injection

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, email_update
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("email_update", email_update, name="email_update"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.db import connection
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from .forms import SignUpForm
 
@@ -9,3 +12,16 @@ class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+@login_required
+def email_update(request):
+    if request.method == "GET":
+        return render(request, "email_update.html")
+    if request.method == "POST":
+        username = request.user
+        email = request.POST.get('email')
+        sql = f"UPDATE auth_user SET email='{email}' WHERE username='{username}'"
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+
+        return redirect("home")
